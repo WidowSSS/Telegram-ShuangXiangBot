7822497d5ce1eeb539e893873b904f0811743cb2
WidowSSS <144976574+WidowSSS@users.noreply.github.com>
2023-09-14 12:51:19 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const sqlite3 = require('sqlite3').verbose();
 
-const { TELEGRAM_BOT_TOKEN, ADMIN_ID, COMMAND_BAN, COMMAND_UNBAN, COMMAND_UNBANTEXT, WELCOME_MESSGAE } = process.env;
+const { TELEGRAM_BOT_TOKEN, ADMIN_ID, COMMAND_BAN, COMMAND_UNBAN, COMMAND_UNBANTEXT, WELCOME_MESSGAE, COMMAND_SEND } = process.env;
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true, request: { family: 4 } });
 const db = new sqlite3.Database('ShuangXiang.db');
 db.run("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, telegram_id INTEGER UNIQUE, telegram_name TEXT, is_blacklisted INTEGER)");
@@ -24,13 +24,37 @@ bot.on('message', (msg) => {
     const { id: telegramId, username: telegramName } = msg.from;
 
     if ('/start'.includes(msg.text)) {
-        if(chatId != ADMIN_ID){
+        if (chatId != ADMIN_ID) {
             const message = `${WELCOME_MESSGAE}`;
             bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
         }
         return;
     }
 
+    if (msg.text.startsWith(COMMAND_SEND)) {
+        const parts = msg.text.split(' ');
+        const args = parts.slice(1);
+        if (chatId == ADMIN_ID) {
+            if (args.length < 1) {
+                const errorMessage = `参数错误，正确格式为：${COMMAND_SEND} + 消息`;
+                bot.sendMessage(chatId, errorMessage);
+            } else {
+                db.all('SELECT telegram_id FROM user', (err, rows) => {
+                    if (err) {
+                        console.error('查询数据库时出错:', err);
+                        return;
+                    }
+                    rows.forEach((row) => {
+                        const userId = row.telegram_id;
+                        if(userId != ADMIN_ID){
+                            sendMessageToUser(userId, args.join(' '));
+                        }
+                    });
+                });
+            }
+        }
+    }
+    
     if (['/ID', '/id'].includes(msg.text)) {
         const message = (chatType === 'group' || chatType === 'supergroup') ?
             `群组用户名: <b>${groupName}</b>\n群组ID: <b>${chatId}</b>` :
@@ -91,4 +115,14 @@ function isUserBlacklisted(telegramId, callback) {
             callback(null, row && row.is_blacklisted === 1);
         }
     });
+}
+
+function sendMessageToUser(userId, messageText) {
+    bot.sendMessage(userId, messageText)
+        .then(() => {
+            console.log(`已向用户 ${userId} 发送消息: ${messageText}`);
+        })
+        .catch((error) => {
+            console.error(`向用户 ${userId} 发送消息时出错:`, error);
+        });
 }
